e7121e2fdd524556c9f9193c48f293f0c40f8b16
Andrey22 <andreymail22112006@gmail.com>
2024-09-30 13:54:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/users", methods=['GET'])
+def main():
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM test')
+
+    data = cur.fetchall()
+
+    data_json = []
+    for d in data:
+        data_json.append(
+            {
+                'id': d[0],
+                'surname': d[1]
+            }
+        )
+
+    cur.close()
+    conn.close()
+
+    return jsonify(data_json)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port="0.0.0.0")
+
