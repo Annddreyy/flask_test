7076198294c95d889869ca3b13272e36547d6af3
Annddreyy <andreymail22112006@gmail.com>
2024-10-06 14:56:51 +0300

    Add get recipe types

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-import sqlite3
-
 from flask import Flask, jsonify
+
+from db import get_connection
 
 app = Flask(__name__)
 
 
 @app.route("/api/v1/recipes", methods=['GET'])
-def main():
-    conn = sqlite3.connect('cooking_app.db')
+def get_recipes():
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute('SELECT * FROM recipe')
@@ -30,6 +30,27 @@ def main():
     conn.close()
 
     return jsonify(recipes_json)
+
+@app.route('/api/v1/recipe_types')
+def get_types():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM recipe_type')
+
+    recipe_types = cur.fetchall()
+
+    recipe_types_json = []
+    for recipe in recipe_types:
+        recipe_types_json.append(
+            {
+                'id': recipe[0],
+                'title': recipe[1],
+                'image_path': recipe[2]
+            }
+        )
+
+    return jsonify(recipe_types_json)
 
 
 if __name__ == '__main__':
